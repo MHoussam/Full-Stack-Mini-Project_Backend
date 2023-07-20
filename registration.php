d7e3b1a67f51b0d8d0cb6f7b1d430acfcba2182c
MHoussam <mohammadosta1999@gmail.com>
2023-07-21 00:30:53 +0300

    Created the registration.php file.

---
+++ registration.php
@@ -0,0 +1,28 @@
+<?php
+include('connection.php');
+
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$check_username = $mysqli->prepare('select username from users where username=?');
+$check_username->bind_param('s', $username);
+$check_username->execute();
+$check_username->store_result();
+$username_exists = $check_username->num_rows();
+
+if ($username_exists == 0) {
+    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+    $query = $mysqli->prepare('insert into users(first_name,last_name,username,password) values(?,?,?,?)');
+    $query->bind_param('ssss', $first_name, $last_name, $username, $hashed_password);
+    $query->execute();
+
+    $response['status'] = "success";
+    $response['message'] = "another message in success";
+} else {
+    $response['status'] = "failed";
+    $response['message'] = "another message in fail";
+}
+
+echo json_encode($response);
